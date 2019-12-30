96e5844afbfbc066c5879943296513321436fedf
ahmed <ahmed_gouda62@yahoo.com>
2019-12-30 09:36:12 -0800

    Initial Commit

---
+++ Auction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auction
+//
+//  Created by Ahmed Gouda on 12/30/19.
+//  Copyright © 2019 Ahmed Gouda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
